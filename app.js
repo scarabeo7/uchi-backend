f601534f2ccdb5cedaf036624986f0dca256ba73
ichinirecords <118000706+ichinirecords@users.noreply.github.com>
2022-12-20 14:46:39 +0000

    Update app.js

--- app.js
+++ app.js
@@ -51,6 +51,8 @@ app.use(passport.initialize());
 app.use(passport.session());
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.setHeader("Access-Control-Allow-Methods", "*");
   next();
 })
 
