46843d1c7af747bedb7a8a256e4b3700b9d72f35
ichinirecords <118000706+ichinirecords@users.noreply.github.com>
2022-12-21 10:56:14 +0000

    Update app.js

--- app.js
+++ app.js
@@ -53,6 +53,7 @@ app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', 'https://uchi.world');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   res.setHeader('Access-Control-Allow-Methods', '*');
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
   next();
 })
 
