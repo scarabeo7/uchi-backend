9707b0d64a782c3358c705d2fcb69c0317323bdc
ichinirecords <118000706+ichinirecords@users.noreply.github.com>
2022-12-21 11:10:26 +0000

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ import path from "path";
 import passport from 'passport';
 import cookieParser from 'cookie-parser';
 import session from 'express-session';
-// import cors from "cors";
+import cors from "cors";
 
 import router from "./api";
 import db from "./db";
@@ -20,9 +20,6 @@ require("./passport")(passport, db);
 // };
 
 const app = express();
-// app.use(cors({
-// 	origin: 'https://goldfish-app-zpg5e.ondigitalocean.app'
-// }));
 
 let sessionOptions = {
   secret: process.env.SESSION_SECRET,
@@ -47,15 +44,18 @@ app.use(logErrors());
 app.use(morgan("dev"));
 app.use(cookieParser());
 app.use(session(sessionOptions));
+app.use(cors({
+	origin: 'https://uchi.world'
+}));
 app.use(passport.initialize());
 app.use(passport.session());
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'https://uchi.world');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  res.setHeader('Access-Control-Allow-Methods', '*');
-  res.setHeader('Access-Control-Allow-Credentials', 'true');
-  next();
-})
+// app.use((req, res, next) => {
+//   res.setHeader('Access-Control-Allow-Origin', 'https://uchi.world');
+//   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+//   res.setHeader('Access-Control-Allow-Methods', '*');
+//   res.setHeader('Access-Control-Allow-Credentials', 'true');
+//   next();
+// })
 
 if (app.get("env") === "production") {
 	app.enable("trust proxy");
